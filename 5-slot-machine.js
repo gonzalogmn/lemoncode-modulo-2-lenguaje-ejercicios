e7ab8bcb3f88d18c4d73c0b8136bc33f44788028
Gonzalo <gonzalo.gimeno.torres@gmail.com>
2019-04-21 16:29:40 +0200

    Reordenación del código

--- 5-slot-machine.js
+++ 5-slot-machine.js
@@ -8,7 +8,6 @@ class SlothMachine {
         this._counter++;
 
         const roulettes = [Math.random() >= 0.5, Math.random() >= 0.5, Math.random() >= 0.5];
-
         console.log(`Roulette results: ${JSON.stringify(roulettes)}`);
 
         const isAWin = roulettes.filter(roulette => roulette == false).length <= 0;
@@ -17,7 +16,6 @@ class SlothMachine {
 
         if(isAWin) {
             message = `Congratulations!!!. You won ${this._counter} coins!!`;
-
             this._counter = 0;
         }
         return message;
