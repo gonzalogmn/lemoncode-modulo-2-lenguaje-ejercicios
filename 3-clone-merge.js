cdfcac6f7a1c2a42a655f1a473a817bd4098339a
Gonzalo <gonzalo.gimeno.torres@gmail.com>
2019-04-21 13:48:30 +0200

    Ejercicio 3

--- 3-clone-merge.js
+++ 3-clone-merge.js
@@ -1,20 +1,40 @@
+// Operadores
+const clone = source => ({...source}); 
+const merge = (source,target) => ({...target, ...source});
+// ----------
+
 let sourceObject = {
     a: 1,
     b: 2
 }
 
-const clone = source => ({...source}); 
-
-
 console.log(`- El objeto original es: ${JSON.stringify(sourceObject)}`);
 
 let clonedObject = clone(sourceObject);
 
-console.log(`- El objeto clonado es: ${JSON.stringify(clonedObject)}`);
+console.log(`- El objeto clonado con el operador clone es: ${JSON.stringify(clonedObject)}`);
 
 sourceObject.a = 3;
 
 console.log(`- Tras modificar el objeto original,
  ahora el objeto original es ${JSON.stringify(sourceObject)} 
- y el objeto clonado ${JSON.stringify(clonedObject)}`);
+ pero el objeto clonado se mantiene como ${JSON.stringify(clonedObject)}`);
 
+
+ const a = {
+     name: "María",
+     surname: "Ibañez",
+     country: "SPA"
+ }
+
+ const b = {
+    name: "Luisa",
+    age: 31,
+    married: true
+ }
+
+ console.log(`- Si tenemos dos objetos como estos:
+  ${JSON.stringify(a)}
+  y ${JSON.stringify(b)}`);
+
+ console.log(`- El objeto creado con el operador merge sobre los dos objetos anteriores es: ${JSON.stringify(merge(a,b))}`);
