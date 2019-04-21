e2bfb74fdf44c4be3f8f5675f4699db4ca0d0ca1
Gonzalo <gonzalo.gimeno.torres@gmail.com>
2019-04-21 13:48:43 +0200

    Reordenación de código

--- 1-array-operations.js
+++ 1-array-operations.js
@@ -1,30 +1,28 @@
-const params = [1,2,3,4];
-
-
+// Operadores
 const head = arr => {
     let [first, ...others] = arr;
     return first;
 }
-
-console.log(`Resultado del operador head con parámetros [${params}]: ${head(params)}`);
-
-
 
 const tail = arr => {
     [first, ...others] = arr;
     return others;
 }
 
+const init = arr => arr.slice(0 , arr.length - 1);
+const last = arr => arr.slice(arr.length - 1 , arr.length);
+// ----------
+
+const params = [1,2,3,4];
+
+
+console.log(`Resultado del operador head con parámetros [${params}]: ${head(params)}`);
+
+
 console.log(`Resultado del operador tail con parámetros [${params}]: ${tail(params)}`);
 
 
-
-const init = arr => arr.slice(0 , arr.length - 1);
-
 console.log(`Resultado del operador init con parámetros [${params}]: ${init(params)}`);
 
-
-
-const last = arr => arr.slice(arr.length - 1 , arr.length);
 
 console.log(`Resultado del operador last con parámetros [${params}]: ${last(params)}`);
--- 2-concat.js
+++ 2-concat.js
@@ -1,10 +1,12 @@
+// Operadores
+const concat = (...params) => [...params];
+// ----------
+
 const param1 = [1,2,3,4];
 const param2 = [7,8,9];
 const param3 = ['a','b','c'];
 const param4 = [true, false];
 
-
-const concat = (...params) => [...params];
 
 console.log(`Resultado del operador concat con parámetros
  [${param1}], [${param2}], [${param3}], [${param4}]:
